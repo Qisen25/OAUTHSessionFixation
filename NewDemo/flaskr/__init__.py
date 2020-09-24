37ccd6f8702faa9bde4969849085095706d6a757
Eleos477 <69184476+Eleos477@users.noreply.github.com>
2020-09-24 16:12:34 +0800

    Changed login form to use WTForms

--- NewDemo/flaskr/__init__.py
+++ NewDemo/flaskr/__init__.py
@@ -2,12 +2,19 @@ import os
 import pickle
 
 from flask import Flask, render_template, request, redirect
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, IntegerField
 
 #App creation
 app = Flask(__name__)
 
 #Package for registration page
 import OAUTH.registration
+
+#Package for objects -- Not yet working correctly
+import OAUTH.objects
+
+#List from old demo that needs to be passed to different modules - Not yet working
+registeredAccounts = []
 
 #Check for existing data & load - can use as standin for database - use code from old demo
 if os.path.isfile('./OAUTHUsers.txt') and os.stat('OAUTHUsers.txt').st_size != 0:
@@ -26,7 +33,18 @@ else:
 #Default page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    #Create login form using above class. Both fields are mandatory
+    login = loginForm(request.form)
+    if request.method == "POST":
+
+        #Debug print
+        print(login.customerNum.data)
+        print(login.password.data)
+
+        #On successful login, will redirect to that user's profile (NOT IMPLEMENTED)
+        return redirect('/login')
+    return render_template('index.html', form=login)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)

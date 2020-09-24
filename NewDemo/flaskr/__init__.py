3a0b6f924a20a4ee056f3846b783e0cf07baed61
Eleos477 <69184476+Eleos477@users.noreply.github.com>
2020-09-24 14:35:27 +0800

    Added code from old demo, moved register route
    Added code to check for existing data from old demo. Moved register route to registration.py

--- NewDemo/flaskr/__init__.py
+++ NewDemo/flaskr/__init__.py
@@ -1,25 +1,32 @@
 import os
+import pickle
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect
 
+#App creation
 app = Flask(__name__)
 
+#Package for registration page
+import OAUTH.registration
+
+#Check for existing data & load - can use as standin for database - use code from old demo
+if os.path.isfile('./OAUTHUsers.txt') and os.stat('OAUTHUsers.txt').st_size != 0:
+
+    loadfile = open('OAUTHUsers.txt', 'rb')
+    registeredUsers = pickle.load(loadfile)
+
+    for item in registeredUsers:
+        registeredAccounts.append(item.account)
+
+    loadfile.close()
+
+else:
+    registeredUsers = []
+
+#Default page
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-#@app.route('/register')
-#def login():
-#    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)

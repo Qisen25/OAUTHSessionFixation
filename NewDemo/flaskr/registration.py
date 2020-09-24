c8dcb6e7d92aa9b6940e7407a175517e653b855f
Eleos477 <69184476+Eleos477@users.noreply.github.com>
2020-09-24 16:17:34 +0800

    Changed register form to WTForms

--- NewDemo/flaskr/registration.py
+++ NewDemo/flaskr/registration.py
@@ -1,19 +1,30 @@
 import os
 
 from OAUTH import app
+from OAUTH import objects
 
 from flask import Flask, render_template, request, redirect
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, IntegerField
+
+#Needs to be passed from main, placed here to temporarily stop errors
+registeredAccounts = []
 
 #Register route
 @app.route('/register', methods=["GET","POST"])
 def register():
 
+    #Generates registration form using above class. All fields are mandatory
+    form = objects.registrationForm(request.form)
     if request.method == "POST":
-        req = request.form
-    
-        #Proof of concept for now
-        print(req)
 
-        return redirect(request.url)
+        #Creates the new user - form data now exists in the user object
+        newUser = objects.user(form.fname.data, form.lname.data, form.password.data, registeredAccounts)
+        
+        #Debug printouts
+        print(newUser.name)
+        print(newUser.surname)
 
-    return render_template('register.html')
+        #Redirects to register complete page on submit
+        return redirect('/register_complete')
+
+    return render_template('register.html', form=form)
